2fc53553eaf5578f0dc067a269d0d314f0582de6
javierMoG <javiermg19978@gmail.com>
2020-11-28 17:29:05 -0600

    primer commit

---
+++ Indice de Bienestar.R
@@ -0,0 +1,13 @@
+library(readxl)
+library(stats)
+#Leemos la base de datos del Banco Mundial
+datosWB <- read_excel("The Last Dance/Aplicada III/Proyecto/Data_Extract_From_World_Development_Indicators .xlsx")
+
+head(datosWB)
+#Tomamos las columnas de interés
+datosAux <- datosWB[,5:20]
+#Nombramos las columnas
+names(datosAux) <- c("CO2", "ER", "PIB", "GH", "EV", "AE", "GINI", "SS", 
+                     "SA", "P", "DE", "EF", "MI", "DEP", "ESC", "DES", "HOM")
+#Matriz de correlación
+cor(datosAux, use ="na.or.complete")
