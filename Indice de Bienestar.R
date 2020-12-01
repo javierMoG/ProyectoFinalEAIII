9c37be16bf88334bff48479824ba46ac01171d78
Andres Cruz <andrscyv@gmail.com>
2020-12-01 15:19:31 -0600

    agrega graficas de componentes principales

--- Indice de Bienestar.R
+++ Indice de Bienestar.R
@@ -4,10 +4,10 @@ library(stats)
 datosWB <- read_excel("The Last Dance/Aplicada III/Proyecto/Data_Extract_From_World_Development_Indicators .xlsx")
 
 head(datosWB)
-#Tomamos las columnas de interés
+#Tomamos las columnas de inter?s
 datosAux <- datosWB[,5:20]
 #Nombramos las columnas
 names(datosAux) <- c("CO2", "ER", "PIB", "GH", "EV", "AE", "GINI", "SS", 
                      "SA", "P", "DE", "EF", "MI", "DEP", "ESC", "DES", "HOM")
-#Matriz de correlación
+#Matriz de correlaci?n
 cor(datosAux, use ="na.or.complete")
